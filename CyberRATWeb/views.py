5a179e6901ef33e7c0250ae0f0f8f328cd5d8c68
Rai <prai@deloitte.ca>
2019-06-06 09:16:17 -0700

    First commit

---
+++ CyberRATWeb/views.py
@@ -0,0 +1,46 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import requests
+
+from CyberRATWeb.models import EmailForm
+
+class Entity():
+    def __init__(self, name, breachNumber, breachedSites):
+        self.name=name
+        self.breachNumber = breachNumber
+        self.breachedSites = breachedSites
+
+def home(request):
+    form = EmailForm()
+    return render(request, 'home.html', {'form': form})
+
+def results(request):
+
+    if request.method == "POST":
+        dataForm = EmailForm(request.POST)
+        if dataForm.is_valid():
+            name = dataForm.cleaned_data['name']
+            email = dataForm.cleaned_data['email']
+        else:
+            dataForm = EmailForm()
+
+    def checkHIBP(email):
+        result= []
+        url = 'https://haveibeenpwned.com/api/v2/breachedaccount/'+email
+
+        resp = requests.get(url=url)
+        try:
+            data = resp.json()
+            for i in range(len(data)):
+                result.append(data[i]['Name'] + " on " + data[i]['BreachDate'] )
+        except:
+            result=[]
+        return result
+
+    entity = Entity('', '', '')
+
+    entity.name = name
+    entity.breachNumber = checkHIBP(email)
+    entity.breachedSites = checkHIBP(email)
+
+    return render(request,'results.html', {'entity':entity})
