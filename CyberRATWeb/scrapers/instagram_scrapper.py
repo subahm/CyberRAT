9fe010ce24bd76c5397aef4763162dd3d4273432
swagner-101 <wagners113@hotmail.com>
2019-06-30 19:33:29 -0700

    added instagram post gatherer

---
+++ CyberRATWeb/scrapers/instagram_scrapper.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import requests
+import time
+from urllib.request import urlopen
+import json
+from .timeline_post import TimelinePost
+
+def get_instagram_posts(profile_link):
+    c = webdriver.Chrome()
+    try:
+        c.get(profile_link)
+
+        element = c.find_element_by_tag_name('body') # or whatever tag you're looking to scrape from
+
+        for i in range(10):
+            element.send_keys(Keys.END)
+            time.sleep(0.5)
+
+        soup = BeautifulSoup(c.page_source, 'html.parser')
+
+        post_urls = []
+        for a in soup.find_all('a'):
+            if '/p/' in a['href']:
+                post_urls.append(profile_link + a['href'])
+
+        result = []
+        for post_url in post_urls:
+            page = urlopen(post_url).read()
+            data = BeautifulSoup(page, 'html.parser')
+            body = data.find('body')
+            script = body.find('script')
+            raw = script.text.strip().replace('window._sharedData =', '').replace(';', '')
+            json_data = json.loads(raw)
+            posts = json_data['entry_data']['PostPage'][0]['graphql']
+            posts = json.dumps(posts)
+            posts = json.loads(posts)
+            result.append(
+                TimelinePost(
+                    posts['shortcode_media']['edge_media_to_caption']['edges'][0]['node']['text'],
+                    posts['shortcode_media']['display_url']
+                )
+            )
+    except:
+        print('failed to obtain posts from instagram timeline')
+        return []
+
+    return result
