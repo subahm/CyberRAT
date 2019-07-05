737148a0777dea09b1ceb55d857400eb32fad9fc
swagner-101 <wagners113@hotmail.com>
2019-07-05 15:07:47 -0700

    added default to new input

--- CyberRATWeb/models.py
+++ CyberRATWeb/models.py
@@ -5,7 +5,7 @@ class Search(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     facebook_link = models.CharField(max_length=200)
-    linkedin_link = models.CharField(max_length=200)
+    linkedin_link = models.CharField(max_length=200, default='')
 
     def get_absolute_url(self):
         return reverse("results", kwargs={'pk':self.pk})
